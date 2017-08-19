c8df3f724d8b8960b13604a18d570bb4a7e61974
jione1 <wldnjs9935@naver.com>
2017-08-20 08:26:03 +0900

    home activity R.java 추가

--- app/src/main/java/com/example/user/devcamp3team/Activity/HomeActivity.java
+++ app/src/main/java/com/example/user/devcamp3team/Activity/HomeActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.example.user.devcamp3team.R;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
