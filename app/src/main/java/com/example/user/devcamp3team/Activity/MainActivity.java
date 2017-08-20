786dd77641901b88d188983949d43bfeeb6de9fe
Jeong Yeo-Eun <jye930217@gmail.com>
2017-08-20 09:05:11 +0900

    main, login 충

--- app/src/main/java/com/example/user/devcamp3team/Activity/MainActivity.java
+++ app/src/main/java/com/example/user/devcamp3team/Activity/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.user.devcamp3team.Activity;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -19,18 +23,20 @@ import com.example.user.devcamp3team.R;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    private MyPagerAdapter mMyPagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.main_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,14 +58,41 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     private void attachTabs() {
-        tabLayout = (TabLayout) findViewById(R.id.main_tabs);
-        tabLayout.addTab(tabLayout.newTab().setText("회계"));
-        tabLayout.addTab(tabLayout.newTab().setText("일정"));
-        tabLayout.addTab(tabLayout.newTab().setText("공지"));
+        mTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        mViewPager = (ViewPager) findViewById(R.id.main_viewpager);
 
-        viewPager = (ViewPager) findViewById(R.id.main_viewpager);
+        mMyPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        mMyPagerAdapter.addFragment(mMyPagerAdapter.getItem(0));
+        mMyPagerAdapter.addFragment(mMyPagerAdapter.getItem(1));
+        mMyPagerAdapter.addFragment(mMyPagerAdapter.getItem(2));
 
-        tabLayout.setupWithViewPager(viewPager);
+        mViewPager.setAdapter(mMyPagerAdapter);
+
+        mTabLayout.addTab(mTabLayout.newTab().setText("회계"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("일정"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("공지"));
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
+//        // Set TabSelectedListener
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+//        mTabLayout.setupWithViewPager(mViewPager);
+
     }
 
     @Override
@@ -81,8 +114,43 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 3;
+        Context mContext;
+//        List<Fragment> mFragments = new ArrayList<>();
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+//            return mFragments.get(position);
+
+            switch (position) {
+                case 0:
+                    return new ScheduleFragment();
+                case 1:
+                    return new ScheduleFragment();
+                case 2:
+                    return new ScheduleFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        public void addFragment(Fragment fragment) {
+//            mFragments.add(fragment);
+        }
     }
 }
