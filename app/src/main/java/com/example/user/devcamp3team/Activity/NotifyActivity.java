db962fc351920ffdba6105302b65fdb67047864f
jione1 <wldnjs9935@naver.com>
2017-09-24 02:47:11 +0900

    새로운 화면 추가

---
+++ app/src/main/java/com/example/user/devcamp3team/Activity/NotifyActivity.java
@@ -0,0 +1,12 @@
+package com.example.user.devcamp3team.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/user/devcamp3team/Activity/NotifyDetailActivity.java
@@ -0,0 +1,12 @@
+package com.example.user.devcamp3team.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotifyDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
